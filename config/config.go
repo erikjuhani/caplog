acd1765fc34a37a4cfc8d7990e4bfd5bcbfadacc
Erik Kinnunen <erik.kinn@gmail.com>
2022-09-04 18:55:46 +0300

    config: add valid configuration paths and refactor
    These paths are `~/.caplog.toml`, `~/.config/caplog.toml` and
    `~/.config/caplog/caplog.toml`. The lookup for existing configuration
    file will return the default location if any of these valid locations do
    not contain existing configuration file.
    
    Change existing variables to more sane defaults.
    
    Add tests to cover config functionality and made logic easier to
    test with parameters and unexported functions.

--- config/config.go
+++ config/config.go
@@ -8,25 +8,37 @@ import (
 	"github.com/pelletier/go-toml/v2"
 )
 
+type ConfigKey = string
+
+// Valid configuration keys
 const (
 	GitLocalRepositoryKey ConfigKey = "git.local_repository"
 	EditorKey             ConfigKey = "editor"
 )
 
+// Default path location constants
 const (
-	configType = "toml"
-
-	defaultDir    = ".caplog"
-	defaultLogDir = "capbook"
+	defaultConfigLocation = "~/.caplog.toml"
+	defaultRepositoryPath = "%s/.caplog/capbook"
 )
+
+// Config is exported to give access to existing configuration values
+var Config = config{}
 
 var (
-	Config     = config{}
+	// In-memory copy of the existing config file
 	configFile []byte
-	configPath string
-)
 
-type ConfigKey = string
+	// The actual path to the nearest config file
+	configPath string
+
+	// Paths that are searched for configuration file
+	validConfigPaths = []string{
+		defaultConfigLocation,
+		"~/.config/caplog.toml",
+		"~/.config/caplog/caplog.toml",
+	}
+)
 
 type gitConfig struct {
 	LocalRepository string `toml:"local_repository,omitempty"`
@@ -37,41 +49,81 @@ type config struct {
 	Editor string     `toml:"editor,omitempty"`
 }
 
-func Load() error {
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return err
+// Load initializes configuration to memory either with default values
+// or read from a `caplog.toml` configuration file if such exists.
+func Load(homeDir string) error {
+	return load(homeDir, &Config)
+}
+
+// Write writes given map structure to a configuration file
+// located in configuration path
+func Write(c map[ConfigKey]string) error {
+	return writeTo(configPath, c)
+}
+
+func findExistingConfigFile(homeDir string) string {
+	for _, v := range validConfigPaths {
+		vv := replaceTilde(v, homeDir)
+		if _, err := os.Stat(vv); os.IsNotExist(err) {
+			continue
+		}
+		return vv
+	}
+	return replaceTilde(defaultConfigLocation, homeDir)
+}
+
+func load(homeDir string, config *config) error {
+	setDefaults(fmt.Sprintf(defaultRepositoryPath, homeDir), config)
+
+	configPath = findExistingConfigFile(homeDir)
+
+	if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+		// Store actual config file to memory if it exists
+		// This is used when merging configurations together
+		// so old configurations are not lost
+		configFile, err = os.ReadFile(configPath)
+		if err != nil {
+			return err
+		}
 	}
 
-	localPath := fmt.Sprintf("%s/%s", homeDir, defaultDir)
-
-	configPath, err = readConfigPath(localPath)
-	if err != nil {
+	if err := toml.Unmarshal(configFile, config); err != nil {
 		return err
 	}
-
-	// Store actual config file to memory
-	configFile, err = os.ReadFile(configPath)
-	if err != nil {
-		return err
-	}
-
-	setDefaults(&Config, localPath)
-
-	if err := toml.Unmarshal(configFile, &Config); err != nil {
-		return err
-	}
-
-	// Replace tilde to point to user home directory
-	Config.Git.LocalRepository = strings.Replace(Config.Git.LocalRepository, "~", homeDir, 1)
 
 	return nil
 }
 
-func Write(c map[ConfigKey]string) error {
+func mergeMapToConfig(c map[ConfigKey]string, config *config) error {
+	for k, v := range c {
+		switch k {
+		case GitLocalRepositoryKey:
+			config.Git = &gitConfig{LocalRepository: v}
+		case EditorKey:
+			config.Editor = v
+		default:
+			return fmt.Errorf("\"%s\" is not a valid configuration key", k)
+		}
+	}
+	return nil
+}
+
+func replaceTilde(s, r string) string {
+	return strings.Replace(s, "~", r, 1)
+}
+
+func setDefaults(rpath string, c *config) {
+	c.Git = &gitConfig{}
+	c.Git.LocalRepository = rpath
+	c.Editor = "vi"
+}
+
+func writeTo(configPath string, c map[ConfigKey]string) error {
 	var localConfig config
-	if err := toml.Unmarshal(configFile, &localConfig); err != nil {
-		return err
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		if err := toml.Unmarshal(configFile, &localConfig); err != nil {
+			return err
+		}
 	}
 
 	if err := mergeMapToConfig(c, &localConfig); err != nil {
@@ -83,44 +135,5 @@ func Write(c map[ConfigKey]string) error {
 		return err
 	}
 
-	return os.WriteFile(configPath, config, 0644)
-}
-
-func mergeMapToConfig(c map[ConfigKey]string, config *config) error {
-	for k, v := range c {
-		switch k {
-		case GitLocalRepositoryKey:
-			config.Git.LocalRepository = v
-		case EditorKey:
-			config.Editor = v
-		default:
-			return fmt.Errorf("\"%s\" is not a valid configuration key", k)
-		}
-	}
-	return nil
-}
-
-func setDefaults(c *config, localPath string) {
-	c.Git = &gitConfig{}
-	c.Git.LocalRepository = fmt.Sprintf("%s/%s", localPath, defaultLogDir)
-	c.Editor = "vi"
-}
-
-// TODO: Add package github.com/adrg/xdg for better configuration support
-// TODO: Search for existing config file in all possible locations
-// on first hit finish
-
-// Viable config paths
-// .config/caplog.toml
-// ~/.caplog.toml
-
-func readConfigPath(localPath string) (string, error) {
-	if _, err := os.Stat(localPath); os.IsNotExist(err) {
-		if err := os.Mkdir(localPath, os.ModePerm); err != nil {
-			return "", fmt.Errorf("cannot create .caplog folder under home directory %w", err)
-		}
-	}
-
-	return fmt.Sprintf("%s/%s", localPath, "config"), nil
-
+	return os.WriteFile(configPath, config[:len(config)-1], 0644)
 }
